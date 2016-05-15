003db3a13740e95061dec70b4de2ae29fc437d9a
RicardoJu <juniorkenobi@gmail.com>
2016-05-15 13:11:38 -0300

    EF

---
+++ EF/AlteraEditoraComEF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EF
+{
+	class AlteraEditoraComEF
+	{
+		static void Main(string[] args)
+		{
+			using(EFContext ctx = new EFContext())
+			{
+				Console.Write("Digite o Id da Editora que deseja alterar:");
+				long id = Convert.ToInt64(Console.ReadLine());
+
+				Editora e = ctx.Editoras.Find(id);
+
+				Console.Write("Digite o novo Nome da Editora:");
+				e.Nome = Console.ReadLine();
+
+				Console.Write("Digite o novo E-mail da Editora:");
+				e.Email = Console.ReadLine();
+
+				ctx.SaveChanges();
+
+	
+			}
+		}
+	}
+}
---
+++ EF/RemoveEditoraComEF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EF
+{
+	class RemoveEditoraComEF
+	{
+		static void Main(string[] args)
+		{
+			using(EFContext ctx = new EFContext())
+			{
+				Console.Write("Digite o Id da Editora que deseja remover:");
+				long id = Convert.ToInt64(Console.ReadLine());
+
+				Editora e = ctx.Editoras.Find(id);
+
+				ctx.Editoras.Remove(e);
+
+				ctx.SaveChanges();
+			}
+		}
+	}
+}
